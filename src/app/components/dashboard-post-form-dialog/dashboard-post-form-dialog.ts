5f3d8a546936f9404fc9a6a497bfa97790d8c2ee
Vinícius Lopes <vinicius.lopes.c@hotmail.com>
2025-09-13 11:20:23 -0300

    feat: finished approach form based on template.

--- src/app/components/dashboard-post-form-dialog/dashboard-post-form-dialog.ts
+++ src/app/components/dashboard-post-form-dialog/dashboard-post-form-dialog.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,10 +7,22 @@ import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-dashboard-post-form-dialog',
-  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
   templateUrl: './dashboard-post-form-dialog.html',
   styleUrl: './dashboard-post-form-dialog.css'
 })
 export class DashboardPostFormDialog {
 
+  author:string = 'Vinícius';
+  title:string = '';
+  date:string = '';
+  content:string = '';
+
+  onSubmit(postForm: NgForm){
+    console.log(`
+      Author: ${this.author} \n
+      Title: ${this.title} \n
+      Date: ${this.date} \n
+      Content: ${this.content}`)
+  }
 }
